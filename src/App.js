323cf63d741bd19e4a16b870a880aa4fc849074d
sandhyab9113 <sandhyab9113@gmail.com>
2021-12-05 20:57:57 +0530

    Router app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import Notfound from './pages/Notfound.js';
+import Post from './pages/Post';
+import Profile from './pages/Profile';
 
 function App() {
+
+  const [login, setlogin] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter >
+      <div className="App">
+        <Header />
+
+
+        <button onClick={() => setlogin(!login)}>{login ? "logout" : "login"}</button>
+
+        <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path='/about' component={About} />
+          <Route path='/profile'>
+            <Profile login={login}/>
+          </Route>
+          <Route path='/post/:id' component={Post} />
+          <Route component={Notfound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
